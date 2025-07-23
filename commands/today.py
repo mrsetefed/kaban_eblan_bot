0aa9c2f4589f37dc39a3a608aac05f3c39b28dd4
mrsetefed <mrsetefed@gmail.com>
2025-07-23 19:32:14 +0500

    1.01.16

--- commands/today.py
+++ commands/today.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 SCHEDULE_URL = "https://raw.githubusercontent.com/mrsetefed/kaban_eblan_bot/refs/heads/schedule/schedules/setefed.json"  # твой json
 
-async def kogda_strad(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def today(update: Update, context: ContextTypes.DEFAULT_TYPE):
     try:
         response = requests.get(SCHEDULE_URL)
         response.raise_for_status()
