e53b4e07c240ffd45586f90a4005fa0de871cfea
mrsetefed <mrsetefed@gmail.com>
2025-07-23 20:37:13 +0500

    1.01.23

--- commands/tomorrow.py
+++
@@ -1,9 +0,0 @@
-from utils import fetch_schedule_json
-from datetime import datetime, timedelta
-
-async def tomorrow(update, context):
-    username = "setefed"  # если нужно — получай из аргументов или профиля
-    schedule = await fetch_schedule_json(username)
-    tomorrow_str = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
-    message = schedule.get(tomorrow_str, "Завтра график не задан")
-    await update.message.reply_text(f"📅 Завтра: {message}")
