584d3223cae984673f1e3beba7134f516f21ae7e
mrsetefed <mrsetefed@gmail.com>
2025-07-23 18:57:30 +0500

    1.01.10

--- commands/today.py
+++ commands/today.py
@@ -4,6 +4,6 @@ from datetime import datetime
 async def today(update, context):
     from datetime import datetime
     today_str = datetime.now().strftime("%Y-%m-%d")
-    schedule = await fetch_json_schedules("setefed")
+    schedule = await fetch_selected_json_schedules("setefed")
     message = schedule.get(today_str, "Сегодня график не задан")
     await update.message.reply_text(f"📅 Сегодня: {message}")
